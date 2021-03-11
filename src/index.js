8b696ea32352c9be9c92c6fe78932efeed66832d
h-fahad <64426351+h-fahad@users.noreply.github.com>
2021-03-10 22:45:44 -0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import "./index.css";
+
+function Room() {
+  const [isLit, setLit] = React.useState(true);
+  const brightness = isLit ? "lit" : "dark";
+  return (
+    <div className={`room ${brightness}`}>
+    the room is {brightness}
+    <br />
+    <button onClick={() => setLit(!isLit)}>
+    flip
+  </button>
+  </div>
+  );
+}
+
+ReactDOM.render(<Room />, document.getElementById('root'));
